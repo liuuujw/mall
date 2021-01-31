ea0cd113b3838ddc709d273c6516ffd63c4fcac5
Kamon <625110949@qq.com>
2021-01-31 23:55:49 +0800

    add vendor directory

---
+++ vendor/topthink/think-captcha/src/helper.php
@@ -0,0 +1,56 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: yunwuxin <448901948@qq.com>
+// +----------------------------------------------------------------------
+
+use think\captcha\facade\Captcha;
+use think\facade\Route;
+use think\Response;
+
+/**
+ * @param string $config
+ * @return \think\Response
+ */
+function captcha($config = null): Response
+{
+    return Captcha::create($config);
+}
+
+/**
+ * @param $config
+ * @return string
+ */
+function captcha_src($config = null): string
+{
+    return Route::buildUrl('/captcha' . ($config ? "/{$config}" : ''));
+}
+
+/**
+ * @param $id
+ * @return string
+ */
+function captcha_img($id = '', $domid = '', $width = '', $height = ''): string
+{
+    $src = captcha_src($id);
+    $style = '';
+    $domid = empty($domid) ? $domid : "id='" . $domid . "'";
+    if($width && $height){
+        $style = "width=\"{$width}\" height=\"{$height}\"";
+    }
+    return "<img src='{$src}' {$style} alt='captcha' " . $domid . " onclick='this.src=\"{$src}?\"+Math.random();' />";
+}
+
+/**
+ * @param string $value
+ * @return bool
+ */
+function captcha_check($value)
+{
+    return Captcha::check($value);
+}
